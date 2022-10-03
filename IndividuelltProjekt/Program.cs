a9436825259836fe5ebe7395fa64ffe9a7828a84
Jonathan Ludvigsson <jonathanludv@gmail.com>
2022-10-03 15:03:59 +0200

    Add project files.

---
+++ IndividuelltProjekt/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Threading.Channels;
+
+namespace IndividuelltProjekt
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+    }
+}
+
